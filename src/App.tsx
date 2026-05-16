2a4257d17ef767ac6a4c896d095d3138b43a29bc
Skip <skip@usecurling.com>
2026-05-16 13:21:20 +0000

    feat: Implement lot economic closure and profitability module

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ import Reclassificacao from './pages/rebanho/Reclassificacao'
 import Reproducao from './pages/reproducao/Reproducao'
 import MetasKPIs from './pages/estrategia/MetasKPIs'
 import SimuladorCenarios from './pages/estrategia/SimuladorCenarios'
-import FechamentoLotes from './pages/estrategia/FechamentoLotes'
+import FechamentoEconomico from './pages/estrategia/FechamentoEconomico'
 import DashboardEstoque from './pages/estoque/DashboardEstoque'
 import PlanejamentoCompras from './pages/estoque/PlanejamentoCompras'
 import CadastroManual from './pages/estoque/CadastroManual'
@@ -404,7 +404,7 @@ const AppRoutes = () => (
         path="/fechamento-lotes"
         element={
           <AuthorizeRoute>
-            <FechamentoLotes />
+            <FechamentoEconomico />
           </AuthorizeRoute>
         }
       />
